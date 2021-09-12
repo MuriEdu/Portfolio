967579d87a7bd9e090cdb2c78ae2b7e3987e1946
Murilo Ramos <muriloeduardo6@gmail.com>
2021-09-12 19:01:22 -0300

    router dom config

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
 function Routes() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/" exact>
           <Home />
@@ -19,7 +18,7 @@ function Routes() {
           <Projects />
         </Route>
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 }
 
